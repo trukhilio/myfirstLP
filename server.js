44af12f7481a55b761ab784a497167f237bdf89a
trukhilio <trukhantech@gmail.com>
2017-04-26 11:47:11 +0300

    changed tree

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/website/build'));
+app.use(express.static(__dirname + '/build/'));
 // set the home page route
 app.get('/', function(req, res) {
     // ejs render automatically looks in the views folder
